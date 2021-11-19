8846af7c9643a2852ff9e60ea77ead4c7ff15669
Kevin Howarth <khowarth422@gmail.com>
2021-11-19 04:27:03 -0500

    Working 1D Update
    Adding working version of 1D PIC algorithms and classes as well as some videos of results.

--- classes.py
+++ classes.py
@@ -1,60 +1,150 @@
-# File containing the grid and particle classes
+# File containing data structures for Electrostatic 1D Particle-in-Cell simulations
 import numpy as np
 import matplotlib.pyplot as plt
+from moviepy.editor import VideoClip
+from moviepy.video.io.bindings import mplfig_to_npimage
 
 # some global constants
-qEle = 1.602176634e-19  # absolute value of the elementary charge [C]
-eMass = 9.10938356e-31  # electron mass [kg]
-eps0 = 8.8541878128e-12  # vacuum permittivity [kg^-1 * m^-3 * s^4 * A^2]
+# NOTE: For now these aren't used as everything is done non-dimensionally (see C below)
+# qEle = 1.602176634e-19  # absolute value of the elementary charge [C]
+# eMass = 9.10938356e-31  # electron mass [kg]
+# eps0 = 8.8541878128e-12  # vacuum permittivity [kg^-1 * m^-3 * s^4 * A^2]
+
+# dictionary of constants for the 1D two-particle case
+# I am copying everything in the dictionary as directly as I can from the textbook
+# In theory if you wanted to change everything to SI units instead of units of
+# plasma frequencies in time, this is where you would enforce that change.
+C = {
+    "Kb": 1.0,  # Boltzmann Constant
+    "eChg": -1.0,  # Electron charge
+    "eMass": 1.0,  # Electron mass
+    "eps0": 1.0,  # Vacuum permittivity
+    "rho0": 1.0,  # Background particle density
+    "T0": 1.0,  # Particle distribution temperature
+    "vd": 1.0  # Particle distribution drift velocity
+}
+C.update({"debyeLength": np.sqrt(C["eps0"]*C["Kb"]/(C["rho0"]*C["eChg"]**2))})
+C.update({"plasmaFreq": np.sqrt(C["rho0"]*C["eChg"]**2/(C["eps0"]*C["eMass"]))})
+C.update({"vTh": C["debyeLength"]*C["plasmaFreq"]})
 
 class Particle:
     # A class for representing a particle with some mass, charge, and kinematic signature
     def __init__(self, ID, x0=0, v0=0):
-        # self.q = q  # Particle charge
-        # self.m = m  # Particle mass
-        self.ID = ID  # string identifier for particle
+        self.ID = ID  # string identifier for particle. Currently not used for anything and may
+                      # not be necessary. I thought it could possibly be useful to carry around
+                      # an identifier, maybe not worth the extra memory especially when we scale
+                      # to large numbers of particles.
         self.x = [x0]  # Position in dimensionless units of intervals [L * H^-1] = [m * m^-1]
         self.v = [v0]  # Velocity in dimensionless units of cell widths per time-step
         self.a = []   # Acceleration in dimensionless units of cell widths per time-step^2
 
 class Grid:
     # A class for representing the 1D grid in which the particles live
-    def __init__(self, L, Ng, dt, T, n0):
+    def __init__(self, L, Ng, dt, T):
         self.L = L  # grid length [m]
         self.H = L/Ng   # grid spacing [m]
         self.Ng = Ng  # number of grid points
-        self.dt = dt  # time-step size [s]
+        self.dt = dt  # time-step size [plasma frequencies]
         self.T = T  # ending simulation time
         self.Particles = []  # list of all Particles in the grid
-        self.Charge = np.zeros(Ng - 1)  # Dimensionless charge at all grid points
-        self.Potential = np.zeros(Ng - 1)  # Dimensionless potential at all grid points
-        self.EField = np.zeros(Ng - 1)  # Dimensionless electric field at all grid points
-        self.n0 = n0  # Background ion density TODO: can maybe estimate rho0 with this?
+                             # TODO: Particles should maybe be a numpy array instead? Does it matter?
+                             #       I'm only using a list so I can do Particles.append() in Grid.addParticle()
+        self.Charge = np.zeros(Ng)  # Dimensionless charge at all grid points
+        self.Potential = np.zeros(Ng)  # Dimensionless potential at all grid points
+        self.EField = np.zeros(Ng)  # Dimensionless electric field at all grid points
+
+        # Populate the dictionary
+        self.C = C.copy()
+        self.C.update({"plasmaFreqDT": self.dt * self.C["plasmaFreq"]})
+        self.C.update({"qBackground": -self.C["plasmaFreqDT"] ** 2 / 2.})
 
     def addParticle(self, p):
+        # Add a particle to the grid. If the particle position is outside the grid, adjust
+        # the position until it is on the periodic image inside the grid.
+        # Note that the valid range of positions is -0.5 <= x < Ng - 0.5, so that the nearest
+        # integer to any particle is a valid grid point index.
+
+        while int(np.round(p.x[0])) >= self.Ng:
+            p.x[0] -= self.Ng
+
+        while int(np.round(p.x[0])) < 0:
+            p.x[0] += self.Ng
+
         self.Particles.append(p)
+
+        # after adding the particle, update the related parameters in the dictionary
+        self.C.update({"avgParticlesPerCell": len(self.Particles)/self.Ng})
+        self.C.update({"delChg": self.C["plasmaFreqDT"]**2 * self.Ng / (2. * len(self.Particles))})
 
     def W(self, x):
         # Charge assignment function, Eq. 2.28 on pg. 31 of Hockney & Eastwood
+        # This is basically the zeroth order interpolation function
         if np.abs(x) <= self.H/2:
             return 1
         else:
             return 0
 
-    def wp(self):
-        # Return the electron plasma frequency
-        return np.sqrt(self.n0*qEle**2/(eps0*eMass))
-
     def plotState1D(self):
         # Plot the position of each particle as a function of time
-        # Not worrying about units for now, just looking at behavior
         for prt in self.Particles:
-            plt.plot(range(len(prt.x)), prt.x, '--', label="Particle "+prt.ID)
+            plt.plot(range(len(prt.x)), prt.x, 'o', label="Particle "+prt.ID)
 
         plt.xlabel("Time-step")
         plt.ylabel("Position [Intervals]")
         plt.title("Particle Positions vs. Time")
         plt.grid()
-        plt.legend(loc='best')
-        # plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
         plt.show()
+
+    def plotCharge1D(self):
+        # Plot the charge at every point on the grid
+        plt.plot(range(self.Ng),self.Charge,'o')
+        plt.xlabel("Grid Position [Intervals]")
+        plt.ylabel("Charge")
+        plt.title("Grid Charge")
+        plt.grid()
+        plt.show()
+
+    def plotEField1D(self):
+        # Plot the charge at every point on the grid
+        plt.plot(range(self.Ng),self.EField,'o')
+        plt.xlabel("Grid Position [Intervals]")
+        plt.ylabel("EField")
+        plt.title("Grid EField")
+        plt.grid()
+        plt.show()
+
+    def plotPotential1D(self):
+        # Plot the potential at every point on the grid
+        plt.plot(range(self.Ng), self.Potential, 'o')
+        plt.xlabel("Grid Position [Intervals]")
+        plt.ylabel("Potential")
+        plt.title("Grid Potential")
+        plt.grid()
+        plt.show()
+
+    def animateState1D(self):
+        # duration of the video
+        duration = 5
+        fps = 20
+
+        # matplot subplot
+        fig, ax = plt.subplots()
+
+        # method to get frames
+        def make_frame(t):
+            # clear
+            ax.clear()
+
+            # plotting line
+            for prt in self.Particles:
+                ax.plot(prt.x[int(t*fps)%len(prt.x)], 0, 'o', label="Particle " + prt.ID)
+                ax.set_xlim([-0.5,63.5])
+
+            # returning numpy image
+            return mplfig_to_npimage(fig)
+
+        # creating animation
+        animation = VideoClip(make_frame, duration=duration)
+
+        # displaying animation with auto play and looping
+        animation.ipython_display(fps=fps, loop=False, autoplay=True)
--- stepper.py
+++ stepper.py
@@ -1,25 +1,30 @@
-# File for performing the time-stepping on a grid
+# File containing algorithms for Electrostatic 1D Particle-in-Cell simulations
 import numpy as np
-from classes import Particle, Grid, qEle
+from numpy.random import MT19937, RandomState, SeedSequence
+from classes import Particle, Grid
 
-def ChargeAssignmentStep(g, Nc):
+def ChargeAssignmentStep(g, debug):
     # Dimensionless Charge Assignment, see pg. 34 of Hockney & Eastwood
 
     # Initialize charge density accumulators
-    for p in range(g.Ng-1):
-        g.Charge[p] = -Nc
+    g.Charge = g.C["qBackground"]*np.ones_like(g.Charge)
 
-    # Accumulate charge density
+    # Accumulate scaled charge density
     for j in range(len(g.Particles)):
         # locate nearest mesh point
         p = int(np.round(g.Particles[j].x[-1]))
 
-        # increment charge density
-        g.Charge[p] += 1
+        if debug:
+            try:
+                # increment charge density
+                g.Charge[p] += g.C["delChg"]
+            except IndexError:
+                print("IndexError for particle with position:",g.Particles[j].x[-1])
+        else:
+            g.Charge[p] += g.C["delChg"]
 
-    # scale charge densities
-    for p in range(g.Ng-1):
-        g.Charge[p] = g.Charge[p] * g.wp()**2 * g.dt**2 / (2 * Nc)
+    if debug:
+        print("Check Charge Neutrality. Sum of charges:",np.sum(g.Charge))
 
 def PoissonStep(g):
     # Solve Poisson's equation to get potential at every point on the grid.
@@ -27,29 +32,30 @@ def PoissonStep(g):
     # is chosen such that the potential is 0 at the 0th grid point.
 
     # Eq. 2.56, compute potential at mesh point 1
-    g.Potential[1] = np.sum([p*g.Charge[p] for p in range(g.Ng-1)])/g.Ng
+    # Slight subtlety here because in Python we are representing mesh points [0 to Ng-1], whereas
+    # in the textbook/Fortran they use base 1 indexing and represent mesh point [1 to Ng]
+    g.Potential[1] = (np.sum([p*g.Charge[p] for p in range(1,g.Ng)]) + g.Ng*g.Charge[0])/g.Ng
 
-    # compute potential at mesh point 2
-    g.Potential[2] = g.Charge[1] + 2*g.Potential[1]
+    # compute remaining potentials
+    for p in range(2, g.Ng):
+        g.Potential[p] = g.Charge[p-1] + 2.*g.Potential[p-1] - g.Potential[p-2]
 
-    # Compute remaining potentials
-    for p in range(3, g.Ng - 1):
-        g.Potential[p] = g.Charge[p-1] + 2*g.Potential[p-1] - g.Potential[p-2]
 
 def EFieldStep(g):
     # Calculate the electric field at every point on the grid using known potentials.
     # Eq. 2-34, pg 32 of Hockney & Eastwood
-    g.EField[0] = g.Potential[1] - g.Potential[-1]
-    for p in range(g.Ng - 2):
-        g.EField[p] = g.Potential[p-1] - g.Potential[p+1]
+    for p in range(g.Ng - 1):
+        g.EField[p] = g.Potential[p+1] - g.Potential[p-1]
     g.EField[-1] = g.Potential[0] - g.Potential[-2]
 
 def ForceInterpStep(g):
     # Dimensionless force interpolation for each particle
     for prt in g.Particles:
-        prt.a.append(0)
-        for p in range(g.Ng - 1):
-            prt.a[-1] += g.W(prt.x[-1] - p)*g.EField[p]
+        # Get nearest mesh point
+        pos = int(np.round(prt.x[-1]))
+
+        # Take E-field at nearest mesh point
+        prt.a.append(g.EField[pos])
 
 def vStep(g):
     # Time-step velocity for each particle
@@ -58,68 +64,118 @@ def vStep(g):
 
 def xStep(g):
     # Time-step position for each particle
+    # TODO: Could add error checking here - eg. if a particle has moved more than
+    #       a few grid lengths in a single time-step, the simulation is probably unstable
     for prt in g.Particles:
         prt.x.append(prt.x[-1] + prt.v[-1])
+        # Enforce periodicity
+        # Translate position outside the right end of the grid
+        while int(np.round(prt.x[-1])) >= g.Ng:
+            prt.x[-1] -= g.Ng
 
-        # enforce periodicity
-        if prt.x[-1] >= 999:
-            prt.x[-1] -= 999
+        # Translate position outside the left end of the grid
+        while int(np.round(prt.x[-1])) < 0:
+            prt.x[-1] += g.Ng
 
-def DiscreteModelStep(g, Nc):
+def DiscreteModelStep(g, debug):
     # Perform a single step through the discrete model on Grid g
     # Charge Assignment
-    ChargeAssignmentStep(g, Nc)
+    if debug:
+        print("Begin Charge Assignment")
+    ChargeAssignmentStep(g, debug)
 
     # Field Equations
+    if debug:
+        print("Begin Poisson Step")
     PoissonStep(g)
+
+    if debug:
+        print("Begin EField Step")
     EFieldStep(g)
 
     # Force Interpolation
+    if debug:
+        print("Begin Force Interpolation")
     ForceInterpStep(g)
 
     # Equations of Motion
+    if debug:
+        print("Begin vStep")
     vStep(g)
+
+    if debug:
+        print("Begin xStep")
     xStep(g)
 
-def RunDiscreteModel(g):
+def RunDiscreteModel(g, debug=False):
     # Time-step the discrete model from 0 to g.T
 
-    # First define relevant parameters
-    rho0 = 1  # background charge density from fixed ions [C*m^-3]
-    Ns = 10  # number of electrons per super-particle [m^-2]
-    Nc = rho0 * g.H / (Ns * qEle)  # average number of super-particles per cell
+    # TODO: Kevin add checking for the four constraints on pg. 32 of Hockney & Eastwood
 
-    # TODO: Check the four constraints on pg. 32 of Hockney & Eastwood
-
-    # Now perform time-stepping
+    # Perform time-stepping
     t = 0
-    while t < g.T:
-        DiscreteModelStep(g, Nc)
-        t += g.dt
+    while t*g.dt < g.T:
+        if debug:
+            print()
+            print("Start step",t)
+        DiscreteModelStep(g, debug)
+        t += 1
 
 if __name__ == '__main__':
-    # Define an example grid
-    L = 1e-7  # Grid length [m]
-    Ng = 1000  # number of cells in grid
-    dt = 1e-5  # time-step size [s]
-    T = 1e-3  # ending simulation time [s]
-    n0 = 1e3  # Background ion density [m^-3]
+    # toggle debug mode
+    debug = True
 
-    G = Grid(L, Ng, dt, T, n0)
+    # Define an example grid
+    L = 64  # Grid length [m]
+    Ng = 64  # number of cells in grid
+    dt = 0.25  # time-step size [s]
+
+    # Run for a few time-steps
+    G = Grid(L=L, Ng=Ng, dt=0.25*dt, T=100*dt)
 
     # Define some example particles and add them to the grid
-    p1 = Particle(ID="1", x0=100, v0=0)
-    p2 = Particle(ID="2", x0=101, v0=0)
-    p3 = Particle(ID="3", x0=102, v0=0)
-    p4 = Particle(ID="4", x0=101.5, v0=0)
-    p5 = Particle(ID="5", x0=102.5, v0=0)
+    p1 = Particle(ID="1", x0=0.5*Ng, v0=0)
+    p2 = Particle(ID="2", x0=0.51*Ng, v0=0)
+    p3 = Particle(ID="3", x0=0.52*Ng, v0=0)
+    p4 = Particle(ID="4", x0=0.49*Ng, v0=0)
+    p5 = Particle(ID="5", x0=0.48*Ng, v0=0)
+    p6 = Particle(ID="6", x0=0.53*Ng, v0=0)
 
     G.addParticle(p1)
     G.addParticle(p2)
     G.addParticle(p3)
     G.addParticle(p4)
     G.addParticle(p5)
+    G.addParticle(p6)
 
-    # Run the simulation and plot the results
-    RunDiscreteModel(G)
-    G.plotState1D()
+    # TODO: Hari simplify this please
+    # set random state for reproducibility when debugging
+    if debug:
+        rs = RandomState(MT19937(SeedSequence(123456789)))
+    else:
+        rs = RandomState()
+
+    # Add a big random distribution of particles
+    for i in range(100):
+        xi = rs.normal(0.6,0.05)
+        G.addParticle(Particle(ID=str(i+7), x0=xi*Ng, v0=0))
+
+    # Define an alternate grid
+    G2 = Grid(L=64, Ng=64, dt=0.25, T=100*0.25)
+    G2.addParticle(Particle("1", x0=10.))
+    G2.addParticle(Particle("2", x0=26.))
+    # G2.addParticle(Particle("2alt", x0=26., v0=3.))  # Can uncomment to try with extra particles
+    # G2.addParticle(Particle("3", x0=43.))  # Replace 2 with 2alt and 3 to recreate "3Particle 1D.mp4"
+
+    # Run the simulation and plot some results
+    # Run with G to test many-particle case
+    # Run with G2 to recreate results on pg 36 of Ch2 of Hockney & Eastwood
+    #    - dt=0.25 recreates Fig. 2.3a as well as "2Particle 1D.mp4"
+    #    - dt=1.0 recreates Fig. 2.3b
+    #    - dt=2.25 recreates Fig. 2.3c
+    RunDiscreteModel(G2, debug=debug)
+    G2.plotCharge1D()
+    G2.plotPotential1D()
+    G2.plotEField1D()
+    G2.plotState1D()
+    # G2.animateState1D()
